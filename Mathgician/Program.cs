88909ad052683ee69e4be05c2de20dd4b66ebcb3
elizabrock <eliza@elizabrocksoftware.com>
2015-01-16 15:42:13 -0600

    Basic Skeleton of Mathgician

---
+++ Mathgician/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathgician
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I AM THE MATHGICIAN");
+            Console.WriteLine("What shall I math?\nFibonacci, Primes, or Integers");
+            string desiredMaths = Console.ReadLine();
+            Console.WriteLine("Ok.  I'm gonna do some " + desiredMaths);
+            if ("Integers" == desiredMaths)
+            {
+                // Do Something Here
+            }
+            else
+            {
+                Console.WriteLine("Well, Actually... I Lied..");
+            }
+            Console.WriteLine("Press Any Key To Exit...");
+            Console.ReadKey();
+        }
+    }
+}
